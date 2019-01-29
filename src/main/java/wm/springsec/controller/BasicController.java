e909f698c29b5a7e39b1decbfe87461048e6d27a
mwisnie <metastable321@gmail.com>
2019-01-29 22:32:02 +0100

    initial commit: simple webmvc with thymeleaf

---
+++ src/main/java/wm/springsec/controller/BasicController.java
@@ -0,0 +1,14 @@
+package wm.springsec.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BasicController {
+
+    @GetMapping("/")
+    public String getHome() {
+        return "home";
+    }
+
+}
